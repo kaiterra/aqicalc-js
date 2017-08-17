5d6c781c56a81ca04fcf269ddcec639ae2372217
Udit <udit@digitalcreative.asia>
2017-08-17 16:38:50 +0800

    change package json name and add the compiled js

---
+++ lib/index.d.ts
@@ -0,0 +1,19 @@
+export interface AirQualityIndexComponents {
+    SO2: number;
+    NO2: number;
+    PM10: number;
+    CO: number;
+    O3: number;
+    PM2_5: number;
+    TVOC: number;
+}
+export interface PrimaryPollutantValue {
+    aqi: number;
+    pollutant: string;
+}
+export interface AirQualityBreakpoints {
+    [key: string]: number[];
+    CN: number[];
+    US: number[];
+}
+export declare const AQICalc: (components: AirQualityIndexComponents, standard: string) => PrimaryPollutantValue[];
